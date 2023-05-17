8b8bc042193f4bec077eba11b16d220982d156d1
hugo-raphael <raphaelhugo15@gmail.com>
2023-05-17 09:54:57 -0300

    alterando algumas informações

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
-  facebookUser: "",
+  nameUser: "Hugo Raphael",
+  githubUser: "hugo-raphael",
+  linkedinUser: "hugo-raphael77",
+  whatsappNumber: "+5532991135337",
+  emailUser: "raphelhugo15@gmail.com",
+  // instagramUser: "",
+  // facebookUser: "",
 };
